b1cd6ef7f87bdde1cf358e930787fe063db3a726
I026 <chiaki.kogretch@gmail.com>
2025-04-04 15:46:05 +0900

    Update slide.js

--- slidepuzzle/systems/js/slide.js
+++ slidepuzzle/systems/js/slide.js
@@ -1436,13 +1436,15 @@ function blockShuffle() {
                     isOperated = true;
                     blockswipeDuration(blockswipeDurationDefault);
                     notificationDisplay(shuffleCompletionMassage(), 0);
-                    waitForElementContent(notificationText, shuffleCompletionMassage(), () => {
-                        notificationText.querySelector(".shuffle_Undo").style.pointerEvents = "auto";
-                        notificationText.querySelector(".shuffle_Undo").addEventListener("click", () => {
-                            recoverFromLocalStorage();
-                            notificationText.querySelector(".shuffle_Undo").style.pointerEvents = "none";
+                    if (notificationText.querySelector(".shuffle_Undo")) {
+                        waitForElementContent(notificationText, shuffleCompletionMassage(), () => {
+                            notificationText.querySelector(".shuffle_Undo").style.pointerEvents = "auto";
+                            notificationText.querySelector(".shuffle_Undo").addEventListener("click", () => {
+                                recoverFromLocalStorage();
+                                notificationText.querySelector(".shuffle_Undo").style.pointerEvents = "none";
+                            });
                         });
-                    });
+                    }
                     steps = 0;
                     if (!(popup[0].classList.contains("popupDisplayAnimation"))) {
                         opacityUndo();
